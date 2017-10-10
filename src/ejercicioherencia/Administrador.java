f5457bda8b2fd2f451ea00f721e705a861bd3e45
Estudiante <Estudiante@10.203.2.39>
2017-10-10 15:03:12 -0500

    Prog Clase

--- src/ejercicioherencia/Administrador.java
+++ src/ejercicioherencia/Administrador.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicioherencia;
 
-/**
- *
- * @author Estudiante
- */
-public class Administrador {
+public class Administrador extends Empleado{
+
+    public Administrador(String nombre, double salario, int id) {
+        super(nombre, 100, id);
+    }
+    
+    @Override
+    public double calcularSalario(){
+        return this.salario;
+    }
+    
+    
     
 }
--- src/ejercicioherencia/Empleado.java
+++ src/ejercicioherencia/Empleado.java
@@ -1,5 +1,32 @@
 package ejercicioherencia;
 
-public abstract class Empleado {
+public abstract class Empleado extends Contrato {
+    protected String nombre;
+    protected double salario;
+
+    public Empleado(String nombre, double salario, int id) {
+        super(id);
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+    
     
 }
