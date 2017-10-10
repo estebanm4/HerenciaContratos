057b71840cf37a5dddfadf5fbb382b79267d9621
Estudiante <Estudiante@10.203.2.39>
2017-10-10 15:20:30 -0500

    Prog Clase

--- src/ejercicioherencia/Compañia.java
+++ src/ejercicioherencia/Compañia.java
@@ -1,5 +1,17 @@
 package ejercicioherencia;
 
+import java.util.ArrayList;
+
 public class Compañia {
+    private String nombre;
+    private ArrayList <Contrato> contratos;
     
+    public Compañia (String nombre){
+        this.nombre = nombre;
+        this.contratos = new ArrayList<>();
+    }
+    
+    public boolean addContrato(Contrato contrato){
+        return this.contratos.add(contrato);          
+    }
 }
